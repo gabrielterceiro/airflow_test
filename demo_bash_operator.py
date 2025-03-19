fbc752274961ef746b2e19d66674e6561cd1abbd
Gabriel Terceiro <gcarolinoterceiro@gmail.com>
2025-03-19 16:18:23 -0300

    Add files via upload

--- demo_bash_operator.py
+++ demo_bash_operator.py
@@ -1,29 +1,40 @@
-import os
-from datetime import timedelta,datetime
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
-#from infra.common import airflow_slack_alerts
-from airflow.operators.bash_operator import BashOperator
 
-DEFAULT_ARGS = {
-  'owner': 'demo',
-  'depends_on_past': False,
-  'start_date': datetime(2024, 5, 1),
-  'email': ['useremail@email.com'],
-  'email_on_failure': True,
-  'email_on_retry': False,
-  'retries': 0,
-  'retry_delay': timedelta(minutes = 5),
-  #'on_failure_callback': airflow_slack_alerts.task_fail_slack_alert
-}
-
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
 with DAG(
-    dag_id="sample_python_dag",default_args=DEFAULT_ARGS,
-    tags=["spcs-demo"],
-    schedule="@once",
+    "tutorial",
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        "depends_on_past": False,
+        "email": ["airflow@example.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+    },
+    description="A simple tutorial DAG",
+    schedule=timedelta(days=1),
+    start_date=datetime(2021, 1, 1),
     catchup=False,
+    tags=["example"],
 ) as dag:
-    
-   task_1 = BashOperator(task_id='sampleTask_bash', 
-                        bash_command = "for i in {1..5}; do sleep 2; echo `date` : Hello World!; done"
-                      )
-   
+
+    # t1, t2 are examples of tasks created by instantiating operators
+    t1 = BashOperator(
+        task_id="print_date",
+        bash_command="date",
+    )
+
+    t2 = BashOperator(
+        task_id="sleep",
+        depends_on_past=False,
+        bash_command="sleep 5",
+        retries=3,
+    )
+    t1 >> t2
