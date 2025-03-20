25767a078b938f381d13d325adeb7297676d9473
Gabriel Terceiro <gcarolinoterceiro@gmail.com>
2025-03-20 11:38:01 -0300

    Add files via upload

--- check_path.py
+++ check_path.py
@@ -8,22 +8,18 @@ DEFAULT_ARGS = {
   'owner': 'demo',
   'depends_on_past': False,
   'start_date': datetime(2024, 5, 1),
-  'email': ['useremail@email.com'],
-  'email_on_failure': True,
-  'email_on_retry': False,
   'retries': 0,
   'retry_delay': timedelta(minutes = 5),
   #'on_failure_callback': airflow_slack_alerts.task_fail_slack_alert
 }
 
 with DAG(
-    dag_id="sample_python_dag",default_args=DEFAULT_ARGS,
+    dag_id="list_path_dag",default_args=DEFAULT_ARGS,
     tags=["spcs-demo"],
     schedule="@once",
     catchup=False,
 ) as dag:
     
-   task_1 = BashOperator(task_id='sampleTask_bash', 
-                        bash_command = "for i in {1..5}; do sleep 2; echo `date` : Hello World!; done"
-                      )
-   
+   run_dbt_model = BashOperator(task_id='list_path', 
+                        bash_command = "printenv && ls -l /snowflake/"
+)
--- dbt_sample_dag.py
+++ dbt_sample_dag.py
@@ -4,26 +4,24 @@ from airflow import DAG
 #from infra.common import airflow_slack_alerts
 from airflow.operators.bash_operator import BashOperator
 
+
 DEFAULT_ARGS = {
   'owner': 'demo',
   'depends_on_past': False,
   'start_date': datetime(2024, 5, 1),
-  'email': ['useremail@email.com'],
-  'email_on_failure': True,
-  'email_on_retry': False,
   'retries': 0,
   'retry_delay': timedelta(minutes = 5),
   #'on_failure_callback': airflow_slack_alerts.task_fail_slack_alert
 }
 
 with DAG(
-    dag_id="sample_python_dag",default_args=DEFAULT_ARGS,
+    dag_id="dbt_python_dag",default_args=DEFAULT_ARGS,
     tags=["spcs-demo"],
     schedule="@once",
     catchup=False,
 ) as dag:
     
-   task_1 = BashOperator(task_id='sampleTask_bash', 
-                        bash_command = "for i in {1..5}; do sleep 2; echo `date` : Hello World!; done"
+   run_dbt_model = BashOperator(task_id='run_dbt_model', 
+                        bash_command = "cd /opt/airflow/dags/dbt/teste_equatorial/ && dbt run --profiles-dir /opt/airflow/dags/dbt/teste_equatorial/"
                       )
    
